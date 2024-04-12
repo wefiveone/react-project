e3b84dced461ec50b78aa5d66db70a7ccd0fbeb6
wefiveone <wefiveone@qq.com>
2024-04-12 20:45:11 +0800

    perf: 性能优化：1）编辑页、统计页路由懒加载；2）webpack配置优化分包出antd、react-dom，合理利用缓存减少首次加载页面main包体积(1.6MB-->27KB)

--- craco.config.js
+++ craco.config.js
@@ -18,5 +18,36 @@ module.exports = {
     //   // webpackConfig.resolve.extensions.push('.ts', '.tsx', 'js', '.jsx')
     //   return webpackConfig
     // }
+    configure(webpackConfig, { env, paths }) {
+      // 生产环境下生效
+      if (env === 'production') {
+        if (webpackConfig.optimization === null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          // 缓存组
+          // 第一次加载的main包中antd、react-dom包占比较大，且为第三方包不常变动，所以可以分离出来单独打包，利用缓存，之后加载时直接从缓存中加载，提升加载速度
+          cacheGroups: {
+            antd: {
+              name: 'antd',
+              test: /[\\/]node_modules[\\/](antd)[\\/]/,
+              priority: 100,
+            },
+            reactDom: {
+              name: 'reactDom',
+              test: /[\\/]node_modules[\\/](react-dom)[\\/]/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 98,
+            }
+          }
+        }
+      }
+      return webpackConfig
+    }
   }
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import ManageLayout from '../layouts/ManageLayout'
@@ -10,8 +10,13 @@ import NotFound from '../pages/NotFound'
 import List from '../pages/manage/List'
 import Star from '../pages/manage/Star'
 import Trash from '../pages/manage/Trash'
-import Edit from '../pages/question/Edit'
-import Stat from '../pages/question/Stat'
+// import Edit from '../pages/question/Edit'
+// import Stat from '../pages/question/Stat'
+
+// 路由懒加载
+const Edit = lazy(() => import(/*webpackChunkName: 'editPage'*/'../pages/question/Edit'))
+const Stat = lazy(() => import(/*webpackChunkName: 'statPage'*/'../pages/question/Stat'))
+
 
 const router = createBrowserRouter([
   {
